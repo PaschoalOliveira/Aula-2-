6bd9a056ed23b6fdb1a20ce499be2a3b55667259
adzcientsalisson <adzcientsalisson@gmail.com>
2020-12-09 10:01:34 -0300

    Alisson commit

---
+++ MembrosApi/Controllers/MembrosController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MembrosApi.Controllers
+{
+    [Route("[controller]")]
+    public class MembrosController : ControllerBase
+    {
+        private static readonly string[] Membros = new[]
+        {
+            "João", "Maria", "Lucas", "Luan", "Laís" 
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public MembrosController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string[] Get()
+        {
+            return Membros;
+        }
+    }
+}
