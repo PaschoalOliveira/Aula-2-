1ef47523236158c842a2db5938ef17a00b2db85a
palomaarize <palomaarize0811@gmail.com>
2020-12-10 15:22:24 -0300

    paloma

--- MembrosApi/Controllers/Teste.cs
+++ MembrosApi/Controllers/Teste.cs
@@ -2,6 +2,16 @@ namespace MembrosApi.Controllers
 {
     public class Teste
     {
+        List<string>  harryPotter = new List<string>()
+        {
+            "Harry",
+            "Hermione",
+            "Rony"
+        };
+
+        List<string> querryAllPersons = from persons in harryPotter
+                                        where persons == "Harry"
+                                        select persons;
         
     }
 }
